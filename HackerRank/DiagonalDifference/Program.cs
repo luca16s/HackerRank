4c1ea0a0de43edf887b622db957965f9077d8ec4
luca16s <luca.gian16@outlook.com>
2017-09-17 11:04:39 -0300

    Added DiagonalDifference.

---
+++ HackerRank/DiagonalDifference/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DiagonalDifference
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var a = new int[n][];
+            for (var aI = 0; aI < n; aI++)
+            {
+                var aTemp = Console.ReadLine().Split(' ');
+                a[aI] = Array.ConvertAll(aTemp, int.Parse);
+            }
+        }
+    }
+}
