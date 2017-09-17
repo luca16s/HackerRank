d57873dd64b54db0741efa90891786f7585aab54
luca16s <luca.gian16@outlook.com>
2017-09-17 12:13:52 -0300

    Closed DiagonalDifference.

--- HackerRank/DiagonalDifference/Program.cs
+++ HackerRank/DiagonalDifference/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DiagonalDifference
 {
@@ -13,6 +14,28 @@ namespace DiagonalDifference
                 var aTemp = Console.ReadLine().Split(' ');
                 a[aI] = Array.ConvertAll(aTemp, int.Parse);
             }
+            var diagonalOne = 0;
+            var diagonalTwo = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                diagonalOne = a[i][i] + diagonalOne;
+            }
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = n-1; j != -1; j--)
+                {
+                    diagonalTwo = a[i][j] + diagonalTwo;
+                    i = i + 1;
+                }
+            }
+            //Console.WriteLine(diagonalOne);
+            //Console.WriteLine(diagonalTwo);
+            Console.WriteLine(Math.Abs(diagonalOne-diagonalTwo));
+            //Console.ReadLine();
+            //  1A - 1 - 1C 00 - 02
+            //  1 - 1B - 1  11 - 11
+            //  1A - 1 - 1C 22 - 20
         }
     }
 }
