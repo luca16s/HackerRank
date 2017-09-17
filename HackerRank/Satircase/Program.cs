2939c5c092388e8cb011f79b0c10b1f175d00cd8
luca16s <luca.gian16@outlook.com>
2017-09-17 14:56:50 -0300

    Edited Staircase

--- HackerRank/Satircase/Program.cs
+++ HackerRank/Satircase/Program.cs
@@ -7,17 +7,8 @@ namespace Satircase
         private static void Main(string[] args)
         {
             var n = Convert.ToInt32(Console.ReadLine());
-            var controle = 1;
             for (var i = 0; i < n; i++)
-            {
-                for (var j = 0; j < controle; j++)
-                {
-                    Console.Write("#");
-                }
-                controle = controle + 1;
-                Console.Write(" ");
-                Console.WriteLine();
-            }
+            Console.WriteLine(new string('#', i+1).PadLeft(n, ' '));
             Console.ReadLine();
         }
     }
