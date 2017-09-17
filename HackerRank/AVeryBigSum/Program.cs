1ac04215e460ee3a1ab319b09e4f2b6af287f287
luca16s <luca.gian16@outlook.com>
2017-09-17 10:39:58 -0300

    Added AVeryBigSum

---
+++ HackerRank/AVeryBigSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AVeryBigSum
+{
+    static class Program
+    {
+        static long AVeryBigSum(int n, long[] ar)
+        {
+            // Complete this function
+        }
+
+        public static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var arTemp = Console.ReadLine().Split(' ');
+            var ar = Array.ConvertAll(arTemp, Int64.Parse);
+            var result = AVeryBigSum(n, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
