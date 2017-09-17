9995546bd3105a6f536c936126dd81d8091c8624
luca16s <luca.gian16@outlook.com>
2017-09-17 12:43:08 -0300

    Finished PlusMinus

--- HackerRank/PlusMinus/Program.cs
+++ HackerRank/PlusMinus/Program.cs
@@ -6,7 +6,33 @@ namespace PlusMinus
     {
         private static void Main(string[] args)
         {
-            
+            var n = Convert.ToInt32(Console.ReadLine());
+            var arrTemp = Console.ReadLine().Split(' ');
+            var arr = Array.ConvertAll(arrTemp, int.Parse);
+
+            double positive = 0, negative = 0, zero = 0;
+            for (var i = 0; i < n; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positive = positive + 1;
+                }
+                if (arr[i] < 0)
+                {
+                    negative = negative + 1;
+                }
+                if (arr[i] == 0)
+                {
+                    zero = zero + 1;
+                }
+            }
+            var totalPlus = positive / n;
+            var totalMinus = negative / n;
+            var totalZero = zero / n;
+            Console.WriteLine(string.Format("{0:0.000000}", totalPlus));
+            Console.WriteLine(string.Format("{0:0.000000}", totalMinus));
+            Console.WriteLine(string.Format("{0:0.000000}", totalZero));
+            Console.ReadLine();
         }
     }
 }
