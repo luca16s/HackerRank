3d399d2ef66ad5f90cc18ff62591b99468a5f4d2
luca16s <luca.gian16@outlook.com>
2017-09-16 23:55:18 -0300

    Added hello, world project.

---
+++ HackerRank/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace HelloWorld
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var inputString = ReadLine();
+
+            WriteLine("Hello, World.");
+            WriteLine(inputString);
+        }
+    }
+}
