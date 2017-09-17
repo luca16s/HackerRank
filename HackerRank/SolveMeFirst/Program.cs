05a0b3c00d17c73bf7551d50f71b6f53ff4020b0
luca16s <luca.gian16@outlook.com>
2017-09-17 00:05:41 -0300

    Added SolveMeFirst project.

---
+++ HackerRank/SolveMeFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    static class Program
+    {
+        private static int SolveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static void Main(string[] args)
+        {
+            var val1 = Convert.ToInt32(Console.ReadLine());
+            var val2 = Convert.ToInt32(Console.ReadLine());
+            var sum = SolveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+    }
+}
