0eef98ec58502a4785775d0d17e98372b5ae770c
luca16s <luca.gian16@outlook.com>
2017-09-17 13:56:28 -0300

    Finished Staircase

--- HackerRank/Satircase/Program.cs
+++ HackerRank/Satircase/Program.cs
@@ -6,7 +6,19 @@ namespace Satircase
     {
         private static void Main(string[] args)
         {
-            
+            var n = Convert.ToInt32(Console.ReadLine());
+            var controle = 1;
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < controle; j++)
+                {
+                    Console.Write("#");
+                }
+                controle = controle + 1;
+                Console.Write(" ");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
