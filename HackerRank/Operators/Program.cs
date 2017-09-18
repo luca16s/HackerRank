6f2a58029944e5ceff0e47e1f50dda8df04d7f93
luca16s <luca.gian16@outlook.com>
2017-09-18 17:07:31 -0300

    Added Operators in C#

---
+++ HackerRank/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operators
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            double mealCost, tip, tax, totalCost, tipPercent, taxPercent;
+            Console.WriteLine("Insert meal cost: ");
+            mealCost = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insert tip percent: ");
+            tipPercent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert Tax Percent: ");
+            taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            tip = mealCost * (tipPercent / 100);
+            tax = mealCost * (taxPercent / 100);
+
+            totalCost = Math.Round(mealCost + tip + tax);
+            Console.WriteLine($"The total meal cost is {totalCost} dollars.");
+            Console.ReadLine();
+        }
+    }
+}
