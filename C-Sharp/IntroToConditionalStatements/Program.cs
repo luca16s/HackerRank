421dfde7b230103ec16f8273dbe54d4cb8a3281a
luca16s <luca.gian16@outlook.com>
2017-09-19 14:02:55 -0300

    Finished

--- C-Sharp/IntroToConditionalStatements/Program.cs
+++ C-Sharp/IntroToConditionalStatements/Program.cs
@@ -6,7 +6,33 @@ namespace IntroToConditionalStatements
     {
         private static void Main(string[] args)
         {
-            
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            if (n%2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                if (n >= 2 && n <= 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else
+                {
+                    if (n >= 6 && n <= 20)
+                    {
+                        Console.WriteLine("Weird");
+                    }
+                    else
+                    {
+                        if (n >= 20)
+                        {
+                            Console.WriteLine("Not Weird");
+                        }
+                    }
+                }
+            }
         }
     }
 }
