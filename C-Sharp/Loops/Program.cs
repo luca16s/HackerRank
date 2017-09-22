c3a0c32574d3b1aa9b373cdd6cd49372975307f4
luca16s <luca.gian16@outlook.com>
2017-09-22 11:41:55 -0300

    Finished Loops

---
+++ C-Sharp/Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Loops
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            for (var i = 1; i < 11; i++)
+            {
+                var result = n * i;
+                Console.WriteLine($"{n} x {i} = {result}");
+            }
+        }
+    }
+}
