f6946f311824033d90ef132180f7fdf477ad2a1b
luca16s <luca.gian16@outlook.com>
2017-09-23 22:19:39 -0300

    Completed Arrays

---
+++ C-Sharp/Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var arrTemp = Console.ReadLine().Split(' ');
+            var arr = Array.ConvertAll(arrTemp, int.Parse);
+            for (var i = n - 1; i > -1; i--)
+            {
+                if (arr.Length > i) Console.Write($"{arr[i]} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
