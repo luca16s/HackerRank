779838eaa56bcb2017772ecd55088b8781d8c4e1
luca16s <luca.gian16@outlook.com>
2017-10-07 12:46:00 -0300

    Add Recursion and Min-MaxSum

---
+++ C-Sharp/Min-MaxSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Min_MaxSum
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var arrTemp = Console.ReadLine().Split(' ');
+            var arr = Array.ConvertAll(arrTemp, int.Parse);
+
+            var firstSum = 0;
+            var secondSum = 0;
+            var thirdSum = 0;
+            var fourthSum = 0;
+            var fifthSum = 0;
+
+            firstSum = arr[1] + arr[2] + arr[3] + arr[4];
+            secondSum = arr[0] + arr[2] + arr[3] + arr[4];
+            thirdSum = arr[0] + arr[1] + arr[3] + arr[4];
+            fourthSum = arr[0] + arr[1] + arr[2] + arr[4];
+            fifthSum = arr[0] + arr[1] + arr[2] + arr[3];
+            
+            if(firstSum < secondSum && firstSum < thirdSum && firstSum < fourthSum && firstSum < fifthSum)
+                Console.Write(firstSum + " ");
+            else if (secondSum < firstSum && secondSum < thirdSum && secondSum < fourthSum && secondSum < fifthSum)
+                Console.Write(secondSum + " ");
+            else if (thirdSum < firstSum && thirdSum < secondSum && thirdSum < fourthSum && thirdSum < fifthSum)
+                Console.Write(secondSum + " ");
+            else if (fourthSum < firstSum && fourthSum < secondSum && fourthSum < thirdSum && fourthSum < fifthSum)
+                Console.Write(fourthSum + " ");
+            else if (fifthSum < firstSum && fifthSum < secondSum && fifthSum < thirdSum && fifthSum < fourthSum)
+                Console.Write(fifthSum + " ");
+
+            if (firstSum > secondSum && firstSum > thirdSum && firstSum > fourthSum && firstSum > fifthSum)
+                Console.Write(firstSum);
+            else if (secondSum > firstSum && secondSum > thirdSum && secondSum > fourthSum && secondSum > fifthSum)
+                Console.Write(secondSum);
+            else if (thirdSum > firstSum && thirdSum > secondSum && thirdSum > fourthSum && thirdSum > fifthSum)
+                Console.Write(secondSum);
+            else if (fourthSum > firstSum && fourthSum > secondSum && fourthSum > thirdSum && fourthSum > fifthSum)
+                Console.Write(fourthSum);
+            else if (fifthSum > firstSum && fifthSum > secondSum && fifthSum > thirdSum && fifthSum > fourthSum)
+                Console.Write(fifthSum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C-Sharp/Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Recursion
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var result = Factorial(n);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static int Factorial(int n)
+        {
+            var counter = n - 1;
+            var result = n;
+            if (n != 1)
+            {
+                result = Factorial(counter) * result;
+            }
+            return result;
+        }
+    }
+}
