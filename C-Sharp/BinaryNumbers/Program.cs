37df75e5f51991aba3eb92ac095d1aa25a6b4c05
luca16s <luca.gian16@outlook.com>
2017-11-05 11:05:29 -0200

    Doing the BinaryNumbers challenge.

---
+++ C-Sharp/BinaryNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryNumbers
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
