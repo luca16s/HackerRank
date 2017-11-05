b754d69a68c686c0b82899dd0d6cbe16323b14d9
luca16s <luca.gian16@outlook.com>
2017-11-05 12:56:35 -0200

    Challenge solved. needs to be evaluated.

--- C-Sharp/BinaryNumbers/Program.cs
+++ C-Sharp/BinaryNumbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BinaryNumbers
 {
@@ -7,6 +9,27 @@ namespace BinaryNumbers
         private static void Main(string[] args)
         {
             var n = Convert.ToInt32(Console.ReadLine());
+            var binaries = new LinkedList<int>();
+            Binarios(n, binaries, 0);
+            Console.WriteLine(Binarios(n, binaries, 0).Item2);
+
+            Console.ReadLine();
+        }
+
+        private static (int, int) Binarios(int number, LinkedList<int> binaries, int count)
+        {
+            var divQuoc = 0;
+            if (number > 0)
+            {
+                var divRest = number % 2;
+                binaries.AddFirst(divRest);
+                divQuoc = number / 2;
+                if (divRest != 0) count++;
+                if (divRest == 0) count--;
+                return Binarios(divQuoc, binaries, count);
+            }
+            return (divQuoc, count);
         }
     }
 }
+//5 - 5%2 =|2|1| - 2%2 = |1|0| - 1%2 = |0|1|
