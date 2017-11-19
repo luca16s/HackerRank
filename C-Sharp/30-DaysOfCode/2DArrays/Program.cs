40c76c91aa477b6008a60e6bbd2da48c10537c73
luca16s <luca.gian16@outlook.com>
2017-11-19 09:56:37 -0200

    Organized Folders.

---
+++ C-Sharp/30-DaysOfCode/2DArrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2DArrays
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new int[6][];
+            for (var arrI = 0; arrI < 6; arrI++)
+            {
+                var arrTemp = Console.ReadLine().Split(' ');
+                arr[arrI] = Array.ConvertAll(arrTemp, int.Parse);
+            }
+        }
+    }
+}
