fa4cf58b66fb7aa60be0ffa73e63c6731bbe66b4
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-06-06 17:33:22 -0300

    - Primeira versão New Year Chaos

---
+++ C-Sharp/HackerRank.Tests/InterviewPreparationKit/HR_01_NewYearChaos_Tests.cs
@@ -0,0 +1,149 @@
+﻿namespace HackerRank.Tests.InterviewPreparationKit
+{
+    using FluentAssertions;
+
+    using HackerRank.InterviewPreparationKit;
+
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using Xunit;
+
+    public class HR_01_NewYearChaos_Tests
+    {
+        [Fact]
+        public void SampleTestCaseNine()
+        {
+            // Arrange
+            int count = 5;
+            string[] inputSource = File
+                .ReadAllText(".\\Resources\\input_sample_nine_NewYearChaos.txt")
+                .Split("-");
+
+            var listOfItems = new List<int>();
+
+            IEnumerable<string> expected = new List<string>
+            {
+                "119847",
+                "Too chaotic",
+                "Too chaotic",
+                "Too chaotic",
+                "119971",
+            };
+
+            // Test
+            var result = new List<string>();
+
+            foreach (string listaItens in inputSource)
+            {
+                result
+                    .Add(HR_01_NewYearChaos
+                    .MinimumBribes(Array.ConvertAll(listaItens.Split(" "), int.Parse).ToList()));
+            }
+
+            // Assert
+            _ = result.Should().StartWith(expected);
+        }
+
+        [Fact]
+        public void SampleTestCaseOne()
+        {
+            // Arrange
+            var caseList = new List<List<int>>
+            {
+                new List<int>{ 5, 1, 2, 3, 7, 8, 6, 4, },
+                new List<int>{ 1, 2, 5, 3, 7, 8, 6, 4, },
+            };
+
+            //12345678|
+            //12354678|1
+            //12534678|2
+            //12534768|3
+            //12537468|4
+            //12537486|5
+            //12537846|6
+            //12537864|7
+
+            IEnumerable<string> expected = new List<string>
+            {
+                "Too chaotic",
+                "7",
+            };
+
+            // Test
+            var result = new List<string>();
+            foreach (List<int> caso in caseList)
+            {
+                result.Add(HR_01_NewYearChaos.MinimumBribes(caso));
+            }
+
+            // Assert
+            _ = result.Should().StartWith(expected);
+        }
+
+        [Fact]
+        public void SampleTestCaseTwo()
+        {
+            // Arrange
+            var caseList = new List<List<int>>
+            {
+                new List<int>{ 1, 2, 5, 3, 4, 7, 8, 6, },
+            };
+
+            //12345678|
+            //12354678|1
+            //12534678|2
+            //12534768|3
+            //12534786|4
+
+            IEnumerable<string> expected = new List<string>
+            {
+                "4",
+            };
+
+            // Test
+            var result = new List<string>();
+            foreach (List<int> caso in caseList)
+            {
+                result.Add(HR_01_NewYearChaos.MinimumBribes(caso));
+            }
+
+            // Assert
+            _ = result.Should().StartWith(expected);
+        }
+
+        [Fact]
+        public void SampleTestCaseZero()
+        {
+            // Arrange
+            var caseList = new List<List<int>>
+            {
+                new List<int>{ 2, 1, 5, 3, 4, },
+                new List<int>{ 2, 5, 1, 3, 4, },
+            };
+
+            //12345|
+            //21345|1
+            //21354|2
+            //21534|3
+
+            IEnumerable<string> expected = new List<string>
+            {
+                "3",
+                "Too chaotic",
+            };
+
+            // Test
+            var result = new List<string>();
+            foreach (List<int> caso in caseList)
+            {
+                result.Add(HR_01_NewYearChaos.MinimumBribes(caso));
+            }
+
+            // Assert
+            _ = result.Should().StartWith(expected);
+        }
+    }
+}
---
+++ C-Sharp/HackerRank/InterviewPreparationKit/HR_01_NewYearChaos.cs
@@ -0,0 +1,39 @@
+﻿namespace HackerRank.InterviewPreparationKit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public class HR_01_NewYearChaos
+    {
+        public static string MinimumBribes(List<int> numbers)
+        {
+            bool tooChaotic = false;
+            int totalOfBribedsPositions = 0;
+            int listCount = numbers.Count;
+
+            for (int i = 0; i < listCount; i++)
+            {
+                int left = numbers[i];
+                int leftActualIndex = numbers.IndexOf(left);
+                int leftMustBeIndex = left - 1;
+
+                if (leftMustBeIndex - leftActualIndex > 2)
+                {
+                    tooChaotic = true;
+                    break;
+                }
+
+                for (int j = Math.Max(0, numbers[i] - 2); j < i; j++)
+                {
+                    if (numbers[j] > numbers[i])
+                        totalOfBribedsPositions++;
+                }
+            }
+
+            return tooChaotic
+                ? "Too chaotic"
+                : $"{totalOfBribedsPositions}";
+        }
+    }
+}
