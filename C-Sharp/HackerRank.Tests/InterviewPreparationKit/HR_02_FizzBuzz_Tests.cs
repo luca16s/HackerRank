67dbaa5dff26336a95f58156ef7eca2b8da1c5e6
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-06-07 19:59:18 -0300

    - Resolução do teste de FizzBuzz

---
+++ C-Sharp/HackerRank.Tests/InterviewPreparationKit/HR_02_FizzBuzz_Tests.cs
@@ -0,0 +1,44 @@
+﻿namespace HackerRank.Tests.InterviewPreparationKit
+{
+    using FluentAssertions;
+
+    using HackerRank.InterviewPreparationKit;
+
+    using System.Collections.Generic;
+
+    using Xunit;
+
+    public class HR_02_FizzBuzz_Tests
+    {
+        [Fact]
+        public void SampleTestCaseOne()
+        {
+            // Arrange
+            int number = 15;
+            string[] expected = new string[]
+            {
+                "1",
+                "2",
+                "Fizz",
+                "4",
+                "Buzz",
+                "Fizz",
+                "7",
+                "8",
+                "Fizz",
+                "Buzz",
+                "11",
+                "Fizz",
+                "13",
+                "14",
+                "FizzBuzz",
+            };
+
+            // Test
+            IEnumerable<string> result = HR_02_FizzBuzz.FizzBuzz(number);
+
+            // Assert
+            _ = result.Should().StartWith(expected);
+        }
+    }
+}
---
+++ C-Sharp/HackerRank/InterviewPreparationKit/HR_02_FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿namespace HackerRank.InterviewPreparationKit
+{
+    using System.Collections.Generic;
+
+    public static class HR_02_FizzBuzz
+    {
+        public static IEnumerable<string> FizzBuzz(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                yield return i % 3 == 0
+                    && i % 5 == 0
+                    ? "FizzBuzz"
+                    : i % 3 == 0
+                    ? "Fizz"
+                    : i % 5 == 0
+                    ? "Buzz"
+                    : $"{i}";
+            }
+        }
+    }
+}
